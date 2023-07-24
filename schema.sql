1a41c829ab01fa30f472a759169fe3dab6fa7824
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-24 20:12:52 -0300

    updated schema to be more readable

--- schema.sql
+++ schema.sql
@@ -1,20 +1,15 @@
-drop database if exists employee_db;
+DROP DATABASE if exists employee_db;
 
-create database employee_db;
+CREATE DATABASE employee_db;
 
-use employee_db;
+USE employee_db;
 
-drop table if exists employee;
-drop table if exists department;
-drop table if exists role;
-drop table if exists budget;
-
-create table department (
+CREATE TABLE department (
     id int auto_increment primary key,
     department_name varchar(100) not null
 );
 
-create table role (
+CREATE TABLE roles (
     id int auto_increment primary key,
     title varchar(100) not null,
     salary decimal not null,
@@ -23,12 +18,12 @@ create table role (
     foreign key (department_id) references department(id)
 );
 
-create table employee (
+CREATE TABLE employees (
     id int auto_increment primary key,
     first_name varchar(100) not null,
     last_name varchar(100) not null,
-    role_id int not null,
+    roles_id int not null,
     manager_id int null,
-    foreign key (role_id) references role(id),
-    foreign key (manager_id) references employee(id)
+    foreign key (roles_id) references roles(id),
+    foreign key (manager_id) references employees(id)
 );
