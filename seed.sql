3ec2236b13ae03760b8fb67e5871447dd2e50c19
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-24 19:24:31 -0300

    added department names to table

---
+++ seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (department_name)
+VALUES
+  ('Engineering'),
+  ('Sales'),
+  ('Marketing'),
+  ('Human Resources'),
+  ('Finance'),
+  ('Customer Service'),
+  ('Project Management'),
+  ('Design'),
+  ('Analytics'),
+  ('Product Development');
