92af340cfe9ba22849640c112d4552059ba91eb9
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-24 20:17:01 -0300

    added roles table seed data

--- seed.sql
+++ seed.sql
@@ -5,8 +5,22 @@ VALUES
   ('Marketing'),
   ('Human Resources'),
   ('Finance'),
-  ('Customer Service'),
-  ('Project Management'),
-  ('Design'),
-  ('Analytics'),
-  ('Product Development');
+  ('Legal');
+
+
+INSERT INTO roles (title, salary, department_id)
+VAlUES
+
+  ('Hardware Engineer', 200000, 1),
+  ('Software Engineer', 200500, 1),
+  ('Sales Lead', 99000, 2),
+  ('Sales Consultant', 82000, 2),
+  ('Chief of Marketing', 118000, 3),
+  ('Senior Marketing Executive', 110000, 3),
+  ('Junior Marketing Executive', 82000, 3),
+  ('Head of HR', 96000, 4),
+  ('HR Advisor', 90000, 4),
+  ('Financial Consultant', 92000, 5),
+  ('Account Manager', 82000, 5),
+  ('Lawyer', 350000, 6),
+  ('Legal Advisor', 150000, 6);
